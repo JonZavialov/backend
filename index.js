2c6493b447afefdbcc33e8b44215c2d708f44f41
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-02-24 11:13:05 -0500

    added some template code for a backend for a blog

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('tiny'))
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(require('./routes/index.routes'))
+app.listen('1337')
