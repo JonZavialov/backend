2c7bd5d1927ca3f231512bf17843f1f7f8d64b64
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-03-01 00:17:37 -0500

    removed api route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 module.exports = router;
-router.use("/api/v1/comments", require("./comment.routes"));
-router.use("/api/v1/oauth", require("./oauth.routes"));
-router.use("/api/v1/github", require("./github.routes"));
+router.use("/v1/comments", require("./comment.routes"));
+router.use("/v1/oauth", require("./oauth.routes"));
+router.use("/v1/github", require("./github.routes"));
