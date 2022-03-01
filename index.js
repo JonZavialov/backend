35c7b36c41aafe68e21d3e21f43494a1291edc5c
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-02-28 23:32:59 -0500

    removed https

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const fs = require("fs");
-var https = require("https");
-var privateKey = fs.readFileSync("sslcert/private.key", "utf8");
-var certificate = fs.readFileSync("sslcert/certificate.crt", "utf8");
 require("dotenv").config();
 
-var credentials = { key: privateKey, cert: certificate };
 const app = express();
 app.use(morgan("tiny"));
 app.use(express.json());
@@ -18,6 +13,4 @@ app.use(
 );
 app.use(cors());
 app.use(require("./routes/index.routes"));
-
-var httpsServer = https.createServer(credentials, app);
-httpsServer.listen(80);
+app.listen(80);
