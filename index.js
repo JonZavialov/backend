a0eae672c2ffa85a58d92b4a287658d1adade6f5
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-02-28 23:29:06 -0500

    made server run with https

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const fs = require("fs");
+var https = require("https");
+var privateKey = fs.readFileSync("sslcert/private.key", "utf8");
+var certificate = fs.readFileSync("sslcert/certificate.crt", "utf8");
 require("dotenv").config();
 
+var credentials = { key: privateKey, cert: certificate };
 const app = express();
 app.use(morgan("tiny"));
 app.use(express.json());
@@ -13,4 +18,6 @@ app.use(
 );
 app.use(cors());
 app.use(require("./routes/index.routes"));
-app.listen("80");
+
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(80);
