066c9438ead8727d5f5d5ab1a254dcdf56c9c67a
Jonathan Zavialov <74842207+JonZavialov@users.noreply.github.com>
2022-03-23 12:21:28 -0400

    Update github.routes.js

--- routes/github.routes.js
+++ routes/github.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const helper = require("../helpers/helper");
 const m = require("../helpers/middlewares");
 
-/* Gets user information from token */
+/* Gets public user information from token */
 router.get("/getUserData", (req, res, next) => m.checkFields(req, res, next, ['token'], true), async (req, res) => {
   // TODO: change this to kebab case
   await helper
